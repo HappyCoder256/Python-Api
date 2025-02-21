e3cc4d80a0d09bc45a70aeea20bf5b06fe9c490d
HappyCoder256 <shamndohjovy256@gmail.com>
2025-02-21 07:48:53 -0800

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,15 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/')
+@app.route('/Home')
 def index():
-    return "Hello World!"
+    return "This is the N9INI API, Thanks for loving and supporting us!"
+@app.route('/direc')
+def index():
+    return "This is the n9ini main_directory lets dive into the deepest world together!"
+@app.route('/movies')
+def index():
+    return "Here is the beauty start of entertainment!"
 
 if __name__ == '__main__':
     app.run()
